6b3abd2341936351730c638dc5caac605e599198
zornfett <18092460nine@gmail.com>
2017-03-17 16:39:49 -0700

    brutal init, brah

---
+++ js/start.brutalizing.js
@@ -0,0 +1,58 @@
+<<<<<<< HEAD
+﻿/* Let the Brutalization Begin */
+/* NOTE: This script is must be UTF-8 encoded, so as to ensure obfuscated character rendering on Baffle */
+
+$(document).ready(function(){
+	
+/* FITTEXT */
+$(".fittext").fitText();
+$(".fittext-compress").fitText(2.2); /* Increases compression */
+$(".fittext-uncompress").fitText(0.8); /* Reduces compression */
+	
+/* Equal Heights */
+$('.equal-height').matchHeight({
+	byRow: true,
+    property: 'height',
+    target: null,
+    remove: false
+});
+	
+/* Prideify - rainbow color filter*/
+$('.pride').prideify();
+	
+/* BAFFLE */
+/* Animation speed value can be increased or decreased. Same with duration */
+let b = baffle('.baffle', { characters: '█▓▒░', speed: 150 }).reveal(2500); /* Runs animation only once, upon page load */
+let bl = baffle('.baffle-longer', { characters: '█▓▒░', speed: 150, duration: 3000 }).reveal(2500); /* Animation duration set to 3 seconds */
+let bf = baffle('.baffle-forever', {characters: '█▓▒░', speed: 150}).start(); /* Animation continues on an endless loop */
+
+=======
+﻿/* Let the Brutalization Begin */
+/* NOTE: This script is must be UTF-8 encoded, so as to ensure obfuscated character rendering on Baffle */
+
+$(document).ready(function(){
+	
+/* FITTEXT */
+$(".fittext").fitText();
+$(".fittext-compress").fitText(2.2); /* Increases compression */
+$(".fittext-uncompress").fitText(0.8); /* Reduces compression */
+	
+/* Equal Heights */
+$('.equal-height').matchHeight({
+	byRow: true,
+    property: 'height',
+    target: null,
+    remove: false
+});
+	
+/* Prideify - rainbow color filter*/
+$('.pride').prideify();
+	
+/* BAFFLE */
+/* Animation speed value can be increased or decreased. Same with duration */
+let b = baffle('.baffle', { characters: '█▓▒░', speed: 150 }).reveal(2500); /* Runs animation only once, upon page load */
+let bl = baffle('.baffle-longer', { characters: '█▓▒░', speed: 150, duration: 3000 }).reveal(2500); /* Animation duration set to 3 seconds */
+let bf = baffle('.baffle-forever', {characters: '█▓▒░', speed: 150}).start(); /* Animation continues on an endless loop */
+
+>>>>>>> 0d7ac9aaeb2f3fcd159ad88832293c32800b43a9
+});
